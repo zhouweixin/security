4be8b4f21e615c5817f807fe58acc470561a7de3
zhouweixin <1216840597@qq.com>
2018-06-12 14:50:08 +0800

    修改了一些bug

--- src/main/java/com/xplusplus/security/SecurityApplication.java
+++ src/main/java/com/xplusplus/security/SecurityApplication.java
@@ -2,15 +2,22 @@ package com.xplusplus.security;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
 
 @SpringBootApplication
-public class SecurityApplication {
+public class SecurityApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder){
+        return builder.sources(SecurityApplication.class);
+    }
     public static void main(String[] args) {
-        SpringApplication.run(SecurityApplication.class, args);
+    	SpringApplication.run(SecurityApplication.class, args);
     }
     
     /**
