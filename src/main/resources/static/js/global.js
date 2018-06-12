52dd9ec18920f9dcbbc6d8677d93769b2990cd8f
chanshibing <40193988+chanshibing@users.noreply.github.com>
2018-06-12 17:01:43 +0800

    Update global.js

--- src/main/resources/static/js/global.js
+++ src/main/resources/static/js/global.js
@@ -1,11 +1,8 @@
-var ipPort = "http://127.0.0.1:9000/security"
-// var ipPort = "http://39.108.89.212:8080/security"
-
-
+ var ipPort = "http://39.108.89.212:8080/security"
+//var ipPort = "http://127.0.0.1:9000/security"
 var departmentsName = []
 var departmentsID = []
-var jobNaturesName = []
-var jobNatureID = []
+
 var positionName = []
 var positionID = []
 var nationName= []
@@ -25,30 +22,30 @@ var personnelContractTypeID = []
 var contractStatusName = []
 var contractStatusID = []
 
-getAllDepartmentsName()
-getAllJobNaturesName()
-getAllPositionName()
-getAllNationName()
-getAllMaritalStatusName()
-getAllMilitaryStatusName()
-getAllPoliticalStatusName()
-getAllHealthStatusName()
-getAllEducationName()
-getAllPersonnelContractTypeName()
-getAllContractStatusName()
+//getAllDepartmentsName()
+// getAllJobNaturesName()
+// getAllPositionName()
+// getAllNationName()
+// getAllMaritalStatusName()
+// getAllMilitaryStatusName()
+// getAllPoliticalStatusName()
+// getAllHealthStatusName()
+// getAllEducationName()
+// getAllPersonnelContractTypeName()
+// getAllContractStatusName()
 
 /*
 获取所有部门名称/
  */
-function getAllDepartmentsName(){
-    var urlStr = ipPort + '/department/getAll'
+function getAllDepartmentsName(department){
     $.ajax({
-        url:urlStr,
+        url:ipPort + '/department/getAll',
         dataType:'json',
         success:function (obj) {
-            for(var i = 0; i < obj.data.length; i++){
-                departmentsName.push(obj.data[i].name)
-                departmentsID.push(obj.data[i].id)
+            for(var i = 1; i < obj.data.length + 1; i ++ ){
+                department.eq(i).parent().removeClass('hidden')
+                department.eq(i).text(obj.data[i - 1].name)
+                department.eq(i).attr('value', obj.data[i - 1].id)
             }
         },
         error:function (error) {
@@ -59,15 +56,15 @@ function getAllDepartmentsName(){
 /*
 获取所有工作性质名称/
  */
-function getAllJobNaturesName(){
-    var urlStr = ipPort + '/jobNature/getAll'
+function getAllJobNaturesName(jobNature){
     $.ajax({
-        url:urlStr,
+        url:ipPort + '/jobNature/getAll',
         dataType:'json',
         success:function (obj) {
-            for(var i = 0; i < obj.data.length; i++){
-                jobNaturesName.push(obj.data[i].name)
-                jobNatureID.push(obj.data[i].id)
+            for(var i = 1; i < obj.data.length + 1; i ++ ){
+                jobNature.eq(i).parent().removeClass('hidden')
+                jobNature.eq(i).text(obj.data[i - 1].name)
+                jobNature.eq(i).attr('value', obj.data[i - 1].id)
             }
         },
         error:function (error) {
@@ -78,15 +75,15 @@ function getAllJobNaturesName(){
 /*
 获取所有职位名称/
  */
-function getAllPositionName(){
-    var urlStr = ipPort + '/role/getAll'
+function getAllPositionName(role){
     $.ajax({
-        url:urlStr,
+        url:ipPort + '/role/getAll',
         dataType:'json',
         success:function (obj) {
-            for(var i = 0; i < obj.data.length; i++){
-                positionName.push(obj.data[i].name)
-                positionID.push(obj.data[i].id)
+            for(var i = 1; i < obj.data.length + 1; i ++ ){
+                role.eq(i).parent().removeClass('hidden')
+                role.eq(i).text(obj.data[i - 1].name)
+                role.eq(i).attr('value', obj.data[i - 1].id)
             }
         },
         error:function (error) {
@@ -97,15 +94,15 @@ function getAllPositionName(){
 /*
 获取所有民族名称/
  */
-function getAllNationName(){
-    var urlStr = ipPort + '/nation/getAll'
+function getAllNationName(nation){
     $.ajax({
-        url:urlStr,
+        url:ipPort + '/nation/getAll',
         dataType:'json',
         success:function (obj) {
-            for(var i = 0; i < obj.data.length; i++){
-                nationName.push(obj.data[i].name)
-                nationID.push(obj.data[i].id)
+            for(var i = 1; i < obj.data.length + 1; i ++ ){
+                nation.eq(i).parent().removeClass('hidden')
+                nation.eq(i).text(obj.data[i - 1].name)
+                nation.eq(i).attr('value', obj.data[i - 1].id)
             }
         },
         error:function (error) {
@@ -116,15 +113,15 @@ function getAllNationName(){
 /*
 获取所有婚姻状况名称/
  */
-function getAllMaritalStatusName(){
-    var urlStr = ipPort + '/maritalStatus/getAll'
+function getAllMaritalStatusName(maritalStatus){
     $.ajax({
-        url:urlStr,
+        url:ipPort + '/maritalStatus/getAll',
         dataType:'json',
         success:function (obj) {
-            for(var i = 0; i < obj.data.length; i++){
-                maritalStatusName.push(obj.data[i].name)
-                maritalStatusID.push(obj.data[i].id)
+            for(var i = 1; i < obj.data.length + 1; i ++ ){
+                maritalStatus.eq(i).parent().removeClass('hidden')
+                maritalStatus.eq(i).text(obj.data[i - 1].name)
+                maritalStatus.eq(i).attr('value', obj.data[i - 1].id)
             }
         },
         error:function (error) {
@@ -135,15 +132,15 @@ function getAllMaritalStatusName(){
 /*
 获取所有兵役状况名称/
  */
-function getAllMilitaryStatusName(){
-    var urlStr = ipPort + '/militaryStatus/getAll'
+function getAllMilitaryStatusName(militaryStatus){
     $.ajax({
-        url:urlStr,
+        url:ipPort + '/militaryStatus/getAll',
         dataType:'json',
         success:function (obj) {
-            for(var i = 0; i < obj.data.length; i++){
-                militaryStatusName.push(obj.data[i].name)
-                militaryStatusID.push(obj.data[i].id)
+            for(var i = 1; i < obj.data.length + 1; i ++ ){
+                militaryStatus.eq(i).parent().removeClass('hidden')
+                militaryStatus.eq(i).text(obj.data[i - 1].name)
+                militaryStatus.eq(i).attr('value', obj.data[i - 1].id)
             }
         },
         error:function (error) {
@@ -154,15 +151,15 @@ function getAllMilitaryStatusName(){
 /*
 获取所有政治面貌名称/
  */
-function getAllPoliticalStatusName(){
-    var urlStr = ipPort + '/politicalStatus/getAll'
+function getAllPoliticalStatusName(politicalStatus){
     $.ajax({
-        url:urlStr,
+        url:ipPort + '/politicalStatus/getAll',
         dataType:'json',
         success:function (obj) {
-            for(var i = 0; i < obj.data.length; i++){
-                politicalStatusName.push(obj.data[i].name)
-                politicalStatusID.push(obj.data[i].id)
+            for(var i = 1; i < obj.data.length + 1; i ++ ){
+                politicalStatus.eq(i).parent().removeClass('hidden')
+                politicalStatus.eq(i).text(obj.data[i - 1].name)
+                politicalStatus.eq(i).attr('value', obj.data[i - 1].id)
             }
         },
         error:function (error) {
@@ -173,15 +170,15 @@ function getAllPoliticalStatusName(){
 /*
 获取所有学历名称/
  */
-function getAllEducationName(){
-    var urlStr = ipPort + '/education/getAll'
+function getAllEducationName(education){
     $.ajax({
-        url:urlStr,
+        url:ipPort + '/education/getAll',
         dataType:'json',
         success:function (obj) {
-            for(var i = 0; i < obj.data.length; i++){
-                educationName.push(obj.data[i].name)
-                educationID.push(obj.data[i].id)
+            for(var i = 1; i < obj.data.length + 1; i ++ ){
+                education.eq(i).parent().removeClass('hidden')
+                education.eq(i).text(obj.data[i - 1].name)
+                education.eq(i).attr('value', obj.data[i - 1].id)
             }
         },
         error:function (error) {
@@ -192,15 +189,15 @@ function getAllEducationName(){
 /*
 获取所有健康状况名称/
  */
-function getAllHealthStatusName(){
-    var urlStr = ipPort + '/healthStatus/getAll'
+function getAllHealthStatusName(healthStatus){
     $.ajax({
-        url:urlStr,
+        url:ipPort + '/healthStatus/getAll',
         dataType:'json',
         success:function (obj) {
-            for(var i = 0; i < obj.data.length; i++){
-                healthStatusName.push(obj.data[i].name)
-                healthStatusID.push(obj.data[i].id)
+            for(var i = 1; i < obj.data.length + 1; i ++ ){
+                healthStatus.eq(i).parent().removeClass('hidden')
+                healthStatus.eq(i).text(obj.data[i - 1].name)
+                healthStatus.eq(i).attr('value', obj.data[i - 1].id)
             }
         },
         error:function (error) {
@@ -211,15 +208,15 @@ function getAllHealthStatusName(){
 /*
 获取所有合同类型名称/
  */
-function getAllPersonnelContractTypeName(){
-    var urlStr = ipPort + '/contractType/getAll'
+function getAllPersonnelContractTypeName(contractType){
     $.ajax({
-        url:urlStr,
+        url:ipPort + '/contractType/getAll',
         dataType:'json',
         success:function (obj) {
-            for(var i = 0; i < obj.data.length; i++){
-                personnelContractTypeName.push(obj.data[i].name)
-                personnelContractTypeID.push(obj.data[i].id)
+            for(var i = 1; i < obj.data.length + 1; i ++ ){
+                contractType.eq(i).parent().removeClass('hidden')
+                contractType.eq(i).text(obj.data[i - 1].name)
+                contractType.eq(i).attr('value', obj.data[i - 1].id)
             }
         },
         error:function (error) {
@@ -228,17 +225,34 @@ function getAllPersonnelContractTypeName(){
     })
 }
 /*
+设置合同类型/
+ */
+function setContractType(contractType, i){
+     $.ajax({
+         url:ipPort + '/contractType/getAll',
+         dataType:'json',
+         success:function (obj) {
+             contractType.val(obj.data[i].name)
+             contractType.attr('value', obj.data[i].id)
+         },
+         error:function (error) {
+             console.log(error)
+         }
+     })
+ }
+
+/*
 获取所有合同状态名称/
  */
-function getAllContractStatusName(){
-    var urlStr = ipPort + '/contractStatus/getAll'
+function getAllContractStatusName(contractStatus){
     $.ajax({
-        url:urlStr,
+        url:ipPort + '/contractStatus/getAll',
         dataType:'json',
         success:function (obj) {
-            for(var i = 0; i < obj.data.length; i++){
-                contractStatusName.push(obj.data[i].name)
-                contractStatusID.push(obj.data[i].id)
+            for(var i = 1; i < obj.data.length + 1; i ++ ){
+                contractStatus.eq(i).parent().removeClass('hidden')
+                contractStatus.eq(i).text(obj.data[i - 1].name)
+                contractStatus.eq(i).attr('value', obj.data[i - 1].id)
             }
         },
         error:function (error) {
@@ -246,3 +260,62 @@ function getAllContractStatusName(){
         }
     })
 }
+ /*
+ 获取所有项目状态名称/
+  */
+ function getAllProjectStatusName(projectStatus){
+     $.ajax({
+         url:ipPort + '/projectStatus/getAll',
+         dataType:'json',
+         success:function (obj) {
+             for(var i = 1; i < obj.data.length + 1; i ++ ){
+                 projectStatus.eq(i).parent().removeClass('hidden')
+                 projectStatus.eq(i).text(obj.data[i - 1].name)
+                 projectStatus.eq(i).attr('value', obj.data[i - 1].id)
+             }
+         },
+         error:function (error) {
+             console.log(error)
+         }
+     })
+ }
+/*
+获取浏览器信息/
+ */
+ function  getExplorer() {
+     var explorer = window.navigator.userAgent ;
+     //ie
+     if (explorer.indexOf("MSIE") >= 0) {
+         return 'ie';
+     }
+     //firefox
+     else if (explorer.indexOf("Firefox") >= 0) {
+         return 'Firefox';
+     }
+     //Chrome
+     else if(explorer.indexOf("Chrome") >= 0){
+         return 'Chrome';
+     }
+     //Opera
+     else if(explorer.indexOf("Opera") >= 0){
+         return 'Opera';
+     }
+     //Safari
+     else if(explorer.indexOf("Safari") >= 0){
+         return 'Safari';
+     }
+ }
+
+ var tableToExcel = (function() {
+     var uri = 'data:application/vnd.ms-excel;base64,',
+         template = '<html><head><meta charset="UTF-8"></head><body><table>{table}</table></body></html>',
+         base64 = function(s) { return window.btoa(unescape(encodeURIComponent(s))) },
+         format = function(s, c) {
+             return s.replace(/{(\w+)}/g,
+                 function(m, p) { return c[p]; }) }
+     return function(table, name) {
+         if (!table.nodeType) table = document.getElementById(table)
+         var ctx = {worksheet: name || 'Worksheet', table: table.innerHTML}
+         window.location.href = uri + base64(format(template, ctx))
+     }
+ })()
