6fa4ccdfa80c786e4814ccbcd1e7f1faa2f3fc84
LittleWhitel <1523673963@qq.com>
2018-06-15 03:13:20 +0800

    小的改动

--- src/main/resources/static/js/attendanceClassManager.js
+++ src/main/resources/static/js/attendanceClassManager.js
@@ -13,10 +13,21 @@ $(document).ready(function () {
 
     })
 
+    $('#select-all').on('click', function () {
+        if(this.checked == true){
+            $('.select-sub-box').prop('checked',true)
+        }
+        else{
+            $('.select-sub-box').prop('checked',false)
+        }
+    })
+    getAllScheduleInformation()
 })
 
 function getAllScheduleInformation() {
-    var page = 0
+    if($('#page_num').val()==''){
+        var page=0
+    }else page =$('#page_num').val();
     var size = 10
     var sortFieldName = 'id'
     var asc = 1
@@ -25,23 +36,26 @@ function getAllScheduleInformation() {
     $.ajax({
         url:urlStr,
         dataType:'json',
-        success:function (obj) {
-            setscheduleTableInformation(obj)
+        cache:false,
+        success: function (obj) {
+           // setScheduleTableInformation(obj)
+          alert(obj.message)
         },
         error:function (error) {
             console.log(error)
+            alert("error")
         }
     })
 }
 
-function setscheduleTableInformation(obj) {
+function setScheduleTableInformation(obj) {
     var table_tr = $('.table-tr')
-    var schedule_id = $('.schedule-id')
-    var schedule_name = $('.schedule-name')
+    var schedule_id = $('.class-id')
+    var schedule_name = $('.class-name')
     for(var i = 0; i < obj.data.numberOfElements; i++){
-        table_tr.eq(i).removeClass('hidden')
-        schedule_id.eq(i).html("<input class=\"select-box select-sub-box\" type=\"checkbox\"" +  "value=\"" + obj.data.content[i].id + "\"" + ">" + obj.data.content[i].id)
-        schedule_name.eq(i).text(obj.data.content[i].name)
+        table_tr.eq(i).removeClass('hidden');
+        schedule_id.eq(i).html("<input class=\"select-box select-sub-box\" type=\"checkbox\"" +  "value='" + obj.data.content[i].id + "'" + ">"+obj.data.content[i].id);
+        schedule_name.eq(i).text(obj.data.content[i].name);
 
     }
     for (var i = obj.data.numberOfElements; i < 10; i++){
--- src/main/resources/static/js/attendanceGroupManager.js
+++ src/main/resources/static/js/attendanceGroupManager.js
@@ -21,20 +21,20 @@ $(document).ready(function () {
 
 })
 
-
+/* *******************************************添加考勤组信息************************************************ */
 function addGroup(){
     var groupName=$('#modal-groupName').val();  // 获取考勤组名称
     //alert(groupName)
     var num1 = $('#lt tr').length
-   // alert(num1)
+  //  alert(num1)
     var num2 = $('#ut tr').length
-   // alert(num2)
+  //  alert(num2)
     var num3 = $('#st tr').length
-   // alert(num3)
+  // alert(num3)
 
     var select_sub_box1 = $('.select-sub-box');
     var jsonArr1=''
-    for(var i = 0; i < num1; i++){
+    for(var i = 0; i < num1-1; i++){
         if(select_sub_box1.eq(i).is(':checked') == true){
             jsonArr1=jsonArr1+select_sub_box1.eq(i).attr('value')+',';
         }
@@ -44,7 +44,7 @@ function addGroup(){
 
     var select_sub_box2 = $('.select-sub-box');
     var jsonArr2 =''
-    for(var i = num1; i < num1+ num2; i++){
+    for(var i = num1-1; i < num1+ num2-2; i++){
         if(select_sub_box2.eq(i).is(':checked') == true){
             jsonArr2=jsonArr2+select_sub_box2.eq(i).attr('value')+',';
         }
@@ -54,7 +54,7 @@ function addGroup(){
 
     var select_sub_box3 = $('.select-sub-box')
     var jsonArr3
-    for(var i = num1+ num2; i < num1+ num2 + num3; i++){
+    for(var i = num1+ num2-3; i < num1+ num2 + num3-3; i++){
         if(select_sub_box3.eq(i).is(':checked') == true){
             jsonArr3 = select_sub_box3.eq(i).attr('value');  // 获取班次编号（ID）
         }
@@ -81,7 +81,8 @@ function addGroup(){
     })
 }
 
-function getAllGroupInformation() {   // 分页  获取所有的考勤组信息
+/* *****************************************获取所有考勤组信息（分页）************************************************** */
+function getAllGroupInformation() {
     if($('#page_num').val()==''){
         var page=0
     }else page =$('#page_num').val();
@@ -102,7 +103,6 @@ function getAllGroupInformation() {   // 分页  获取所有的考勤组信息
         }
     })
 }
-
 function setAllGroupInformation(obj) {    //  将考勤组信息填入表中
     var table_tr = $('.table-tr');
     var group_id = $('.group-id');
@@ -124,8 +124,8 @@ function setAllGroupInformation(obj) {    //  将考勤组信息填入表中
 
 }
 
-
-function searchByName() {   // 通过名称获取考勤组信息
+/* ******************************通过名称获取考勤组信息（模糊搜索）****************************** */
+function searchByName() {
     if($('#page_num').val()==''){
         var page=0
     }else page =$('#page_num').val();
@@ -148,7 +148,8 @@ function searchByName() {   // 通过名称获取考勤组信息
     })
 }
 
-function getAllUserInformation1() {  // 添加考勤组  分页  获取所有员工的信息
+/* ******************************添加考勤组----获取所有（员工）负责人的信息（分页 ）****************************** */
+function getAllUserInformation1() {
     if($('#page_num1').val()==''){
         var page=0
     }else page =$('#page_num1').val();
@@ -170,7 +171,6 @@ function getAllUserInformation1() {  // 添加考勤组  分页  获取所有员
         }
     })
 }
-
 function setUserInformation1(obj) {  // 将员工信息填入表中
     var table_tr = $('.table-tr1');
     var leader_id = $('.leader-id1');
@@ -186,7 +186,8 @@ function setUserInformation1(obj) {  // 将员工信息填入表中
     }
 }
 
-function getAllUserInformation2() {  // 添加考勤组  分页  获取所有员工的信息
+/* ********************************************添加考勤组----获取所有员工的信息（分页 ）******************************************* */
+function getAllUserInformation2() {
     if($('#page_num2').val()==''){
         var page=0
     }else page =$('#page_num2').val();
@@ -222,63 +223,8 @@ function setUserInformation2(obj) {  // 将员工信息填入表中
     }
 }
 
-
-
-function deleteGroup(thisObj) {   // 通过考勤组ID删除考勤组
-    var td = $(thisObj).parent().parent().find('td')
-    var groupId = td.eq(0).text()
-    var urlStr = 'http://127.0.0.1:9000/security/attendanceGroup/deleteById?id='+ groupId;
-    alert(urlStr);
-    $.ajax({
-        url:urlStr,
-        dataType:'json',
-        success:function (obj) {
-            if(obj.code == 0){
-                alert("删除信息成功！");
-                setAllGroupInformation(obj)
-            }else {
-                alert(obj.message)
-            }
-        },
-        error:function (error) {
-            console.log(error)
-        }
-    })
-
-}
-
-function getGroupUsers(){   // 修改考勤组   查看某个考勤组中的员工及负责人
-    var groupid= $('#modal-modifygroupId');
-    var urlStr = 'http://127.0.0.1:9000/security/attendanceGroup/getUsers?id='+ groupid ;
-    alert(urlStr);
-    $.ajax({
-        url:urlStr,
-        dataType:'json',
-        cache:false,
-        success:function (obj) {
-            setGroupUsers(obj)
-        },
-        error:function (error) {
-            console.log(error)
-        }
-    })
-}
-function setGroupUsers(){
-    var table_tr = $('.table-tr3');
-    var user_id = $('.user-id2');
-    var user_name = $('.user-name2');
-    for (var i = 0; i < obj.data.numberOfElements; i++) {
-        table_tr.eq(i).removeClass('hidden');
-        user_id.eq(i).html(obj.data.content[i].id);
-        user_name.eq(i).text(obj.data.content[i].name);
-
-    }
-    for (var i = obj.data.numberOfElements; i < 10; i++) {
-        table_tr.eq(i).addClass('hidden')
-    }
-}
-
-function getAllschedules(){  // 添加考勤组  获取所有的班次信息
+/* **********************************************添加考勤组----获取所有的班次信息（分页 ）******************************************* */
+function getAllschedules(){
     if($('#page_num3').val()==''){
         var page=0
     }else page =$('#page_num3').val();
@@ -286,7 +232,7 @@ function getAllschedules(){  // 添加考勤组  获取所有的班次信息
     var sortFieldName = 'id';
     var asc = 1;
     var urlStr = 'http://127.0.0.1:9000/security/schedule/getAllByPage?page= '+ page + '&size=' + size + '&sortFieldName=' + sortFieldName + '&asc=' + asc;
-   // alert(urlStr);
+    // alert(urlStr);
     $.ajax({
         url:urlStr,
         dataType:'json',
@@ -314,39 +260,224 @@ function setAllschedules(obj){
     }
 }
 
-/*
-修改考勤组信息/
- */
-/*
+
+/* ***********************************************通过ID删除考勤组**************************************************** */
+function deleteGroup(thisObj) {
+    var td = $(thisObj).parent().parent().find('td')
+    var groupId = td.eq(0).text()
+    var urlStr = 'http://127.0.0.1:9000/security/attendanceGroup/deleteById?id='+ groupId;
+    alert(urlStr);
+    $.ajax({
+        url:urlStr,
+        dataType:'json',
+        success:function (obj) {
+            if(obj.code == 0){
+                alert("删除信息成功！");
+                setAllGroupInformation(obj)
+            }else {
+                alert(obj.message)
+            }
+        },
+        error:function (error) {
+            console.log(error)
+        }
+    })
+
+}
+
+/* ******************************修改考勤组----获取所有（员工）负责人的信息（分页 ）****************************** */
+function getAllgUserInformation1() {
+    if($('#page_num4').val()==''){
+        var page=0
+    }else page =$('#page_num4').val();
+    var size = 5;
+    var sortFieldName = 'id';
+    var asc = 1;
+    var urlStr = 'http://127.0.0.1:9000/security/user/getAllByPage?page= '+ page + '&size=' + size + '&sortFieldName=' + sortFieldName + '&asc=' + asc;
+    // alert(urlStr);
+    $.ajax({
+        url:urlStr,
+        dataType:'json',
+        cache:false,
+        success:function (obj) {
+            setgUserInformation1(obj)
+
+        },
+        error:function (error) {
+            console.log(error)
+        }
+    })
+}
+function setgUserInformation1(obj) {  // 将员工信息填入表中
+    var table_tr = $('.table-tr5');
+    var leader_id = $('.leader-id2');
+    var leader_name = $('.leader-name2');
+
+    for (var i = 0; i < obj.data.numberOfElements; i++) {
+        table_tr.eq(i).removeClass('hidden');
+        leader_id.eq(i).html("<input class=\"select-box select-sub-box\" type=\"checkbox\"" +  "value='" + obj.data.content[i].id + "'" + ">"+obj.data.content[i].id);
+        leader_name.eq(i).text(obj.data.content[i].name);
+    }
+    for (var i = obj.data.numberOfElements; i < 5; i++) {
+        table_tr.eq(i).addClass('hidden')
+    }
+}
+
+/* ********************************************修改考勤组----获取所有员工的信息（分页 ）******************************************* */
+function getAllgUserInformation2() {
+    if($('#page_num5').val()==''){
+        var page=0
+    }else page =$('#page_num5').val();
+    var size = 5;
+    var sortFieldName = 'id';
+    var asc = 1;
+    var urlStr = 'http://127.0.0.1:9000/security/user/getAllByPage?page= '+ page + '&size=' + size + '&sortFieldName=' + sortFieldName + '&asc=' + asc;
+    // alert(urlStr);
+    $.ajax({
+        url:urlStr,
+        dataType:'json',
+        cache:false,
+        success:function (obj) {
+            setgUserInformation2(obj)
+
+        },
+        error:function (error) {
+            console.log(error)
+        }
+    })
+}
+function setgUserInformation2(obj) {  // 将员工信息填入表中
+    var table_tr = $('.table-tr6');
+    var user_id = $('.user-id2');
+    var user_name = $('.user-name2');
+    for (var i = 0; i < obj.data.numberOfElements; i++) {
+        table_tr.eq(i).removeClass('hidden');
+        user_id.eq(i).html( "<input class=\"select-box select-sub-box\" type=\"checkbox\"" +  "value='" +obj.data.content[i].id + "'" + ">"+obj.data.content[i].id)
+        user_name.eq(i).text(obj.data.content[i].name);
+    }
+    for (var i = obj.data.numberOfElements; i < 5; i++) {
+        table_tr.eq(i).addClass('hidden')
+    }
+}
+
+
+/* ******************************修改考勤组----通过考勤组ID获取所有负责人的信息****************************** */
+function getGroupLeaders(){
+    var td = $(obj).parent().parent().find('td')
+    $('#modal-modifygroupID').val(td.eq(0).text())
+    var groupid= $('#modal-modifygroupId');
+    var urlStr = 'http://127.0.0.1:9000/security/attendanceGroup/getLeaders?id='+ groupid ;
+    alert(urlStr);
+    $.ajax({
+        url:urlStr,
+        dataType:'json',
+        cache:false,
+        success:function (obj) {
+            setGroupLeaders(obj)
+        },
+        error:function (error) {
+            console.log(error)
+        }
+    })
+}
+function setGroupLeaders(obj){
+    var table_tr = $('.table-tr5');
+    var user_id = $('.user-id2');
+    var user_name = $('.user-name2');
+    for (var i = 0; i < obj.data.length; i++) {
+        table_tr.eq(i).removeClass('hidden');
+        user_id.eq(i).html("<input class=\"select-box select-sub-box\" type=\"checkbox\"" +  "value='" +obj.data[i].leader.id + "'" + ">"+obj.data[i].leader.id);
+        user_name.eq(i).text(obj.data[i].leader.name);
+
+    }
+    for (var i = obj.data.length; i < 5; i++) {
+        table_tr.eq(i).addClass('hidden')
+    }
+}
+
+/* ******************************修改考勤组----通过考勤组ID获取所有员工的信息（分页 ）****************************** */
+function getGroupUsers(){
+    var groupid= $('#modal-modifygroupId');
+    var urlStr = 'http://127.0.0.1:9000/security/attendanceGroup/getUsers?id='+ groupid ;
+    alert(urlStr);
+    $.ajax({
+        url:urlStr,
+        dataType:'json',
+        cache:false,
+        success:function (obj) {
+            setGroupUsers(obj)
+        },
+        error:function (error) {
+            console.log(error)
+        }
+    })
+}
+function setGroupUsers(obj){
+    var table_tr = $('.table-tr6');
+    var user_id = $('.user-id2');
+    var user_name = $('.user-name2');
+    for (var i = 0; i < obj.data.length; i++) {
+        table_tr.eq(i).removeClass('hidden');
+        user_id.eq(i).html("<input class=\"select-box select-sub-box\" type=\"checkbox\"" +  "value='" +obj.data[i].id + "'" + ">"+obj.data[i].id);
+        user_name.eq(i).text(obj.data[i].name);
+
+    }
+    for (var i = obj.data.length; i < 15; i++) {
+        table_tr.eq(i).addClass('hidden')
+    }
+}
+
+/* ******************************修改考勤组----获取被修改考勤组的某些关键信息****************************** */
 function setmodifyGroupInformation(obj) {
     var td = $(obj).parent().parent().find('td')
     $('#modal-modifygroupId').val(td.eq(0).text())
     $('#modal-modifygroupName').val(td.eq(1).text())
+  //  getGroupLeaders(obj)
+  //  getGroupUsers(obj)
+
 }
 
-function setmodifyUserInformation(obj) {
-    var td = $(obj).parent().parent().find('td')
-    $('#modal-modifygroupId').val(td.eq(0).text())
-    $('#modal-modifygroupName').val(td.eq(1).text())
-}
-function setmodifyLeaderInformation(obj) {
-    var td = $(obj).parent().parent().find('td')
-    $('#modal-modifygroupId').val(td.eq(0).text())
-    $('#modal-modifygroupName').val(td.eq(1).text())
-}
-
+/* ******************************修改考勤组----重新调整考勤组信息并交后台处理****************************** */
 function modifyGroup() {
-    var departmentID = $('#modal-modifyDepartmentID').val()
-    var departmentName = $('#modal-modifyDepartmentName').val()
-    var departmentShortName = $('#modal-modifyDepartmentShortName').val()
-    var departmentDescription = $('#modal-modifyDepartmentDescription').val()
-    var urlStr = ipPort + '/department/update?id='+ departmentID + "&name=" + departmentName + "&shortName=" + departmentShortName + "&description=" + departmentDescription
+    var groupID = $('#modal-modifygroupId').val()
+    alert(groupID)
+    var groupName=$('#modal-groupName').val();  // 获取考勤组名称
+    alert(groupName)
+    var num1 = $('#lt tr').length
+    alert(num1)
+    var num2 = $('#ut tr').length
+    alert(num2)
+
+
+    var select_sub_box1 = $('.select-sub-box');
+    var jsonArr1=''
+    for(var i = 0; i < num1-1; i++){
+        if(select_sub_box1.eq(i).is(':checked') == true){
+            jsonArr1=jsonArr1+select_sub_box1.eq(i).attr('value')+',';
+        }
+    }
+    var leaders=jsonArr1.substring(0,jsonArr1.length-1)      //获取负责人 IDS
+    alert(leaders)
+
+    var select_sub_box2 = $('.select-sub-box');
+    var jsonArr2 =''
+    for(var i = num1-1; i < num1+ num2-2; i++){
+        if(select_sub_box2.eq(i).is(':checked') == true){
+            jsonArr2=jsonArr2+select_sub_box2.eq(i).attr('value')+',';
+        }
+    }
+    var users=jsonArr2.substring(0,jsonArr2.length-1)  //获取员工 IDS
+    alert(users)
+
+
+    var urlStr = ipPort + '/department/update?id='+ groupID + "&ids=" + users + "&leadersId=" + leaders
+    alert(urlStr)
     $.ajax({
         url:urlStr,
         dataType:'json',
         success:function (obj) {
             if(obj.code == 8){
-                alert('修改失败, 部门简称只能是唯一的2位字母！')
+                alert('修改失败!')
             }
             else if(obj.code == 0){
                 alert("修改部门信息成功！")
@@ -358,7 +489,8 @@ function modifyGroup() {
         }
     })
 }
-*/
+
+
 
 
 
