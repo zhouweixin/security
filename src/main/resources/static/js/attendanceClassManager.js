40555a397df6be383be278b79038deba12505f9f
LittleWhitel <1523673963@qq.com>
2018-06-15 18:46:08 +0800

    小的改动

--- src/main/resources/static/js/attendanceClassManager.js
+++ src/main/resources/static/js/attendanceClassManager.js
@@ -1,17 +1,17 @@
 $(document).ready(function () {
-    var activeNumbersOfWork
+    var activeNumbersOfWork;
     $('.numbersOfWork').on('click', function () {
         if(!activeNumbersOfWork){
-            activeNumbersOfWork = $(this)
+            activeNumbersOfWork = $(this);
             activeNumbersOfWork.addClass('active')
         }else{
-            var previousNumbersOfWork = activeNumbersOfWork
-            previousNumbersOfWork.removeClass('active')
-            activeNumbersOfWork = $(this)
+            var previousNumbersOfWork = activeNumbersOfWork;
+            previousNumbersOfWork.removeClass('active');
+            activeNumbersOfWork = $(this);
             activeNumbersOfWork.addClass('active')
         }
 
-    })
+    });
 
     $('#select-all').on('click', function () {
         if(this.checked == true){
@@ -20,19 +20,19 @@ $(document).ready(function () {
         else{
             $('.select-sub-box').prop('checked',false)
         }
-    })
+    });
     getAllScheduleInformation()
-})
+});
 
 function getAllScheduleInformation() {
     if($('#page_num').val()==''){
         var page=0
     }else page =$('#page_num').val();
-    var size = 10
-    var sortFieldName = 'id'
-    var asc = 1
-    var urlStr =  'http://127.0.0.1:9000/security/schedule/getAllByPage?page='+ page + '&size=' + size + '&sortFieldName=' + sortFieldName + '&asc=' + asc
-    alert(urlStr)
+    var size = 10;
+    var sortFieldName = 'id';
+    var asc = 1;
+    var urlStr =  'http://39.108.89.212:8080/security/schedule/getAllByPage?page='+ page + '&size=' + size + '&sortFieldName=' + sortFieldName + '&asc=' + asc;
+  //  alert(urlStr)
     $.ajax({
         url:urlStr,
         dataType:'json',
@@ -42,16 +42,16 @@ function getAllScheduleInformation() {
          // alert(obj.message)
         },
         error:function (error) {
-            console.log(error)
+            console.log(error);
             alert("error")
         }
     })
 }
 
 function setScheduleTableInformation(obj) {
-    var table_tr = $('.table-tr')
-    var schedule_id = $('.class-id')
-    var schedule_name = $('.class-name')
+    var table_tr = $('.table-tr');
+    var schedule_id = $('.class-id');
+    var schedule_name = $('.class-name');
     for(var i = 0; i < obj.data.numberOfElements; i++){
         table_tr.eq(i).removeClass('hidden');
         schedule_id.eq(i).html("<input class=\"select-box select-sub-box\" type=\"checkbox\"" +  "value='" + obj.data.content[i].id + "'" + ">"+obj.data.content[i].id);
@@ -65,16 +65,18 @@ function setScheduleTableInformation(obj) {
 
 
 function deleteSchedule(thisObj) {
-    var td = $(thisObj).parent().parent().find('td')
-    var scheduleID = td.eq(0).text()
-    var urlStr =  'http://127.0.0.1:9000/security/schedule/deleteById?id='+ scheduleID
+    var td = $(thisObj).parent().parent().find('td');
+    var scheduleID = td.eq(0).text();
+   // alert(scheduleID)
+    var urlStr =  'http://39.108.89.212:8080/security/schedule/deleteById?id='+ scheduleID;
+   // alert(urlStr)
     $.ajax({
         url:urlStr,
         dataType:'json',
         success:function (obj) {
             if(obj.code == 0){
-                alert("删除班次信息成功！")
-                getAllDepartmentInformation()
+                alert("删除班次信息成功！");
+                getAllScheduleInformation()
             }else {
                 alert(obj.message)
             }
@@ -85,3 +87,108 @@ function deleteSchedule(thisObj) {
     })
 }
 
+function addSchedule() {
+    var className = $('#modal-className').val();
+    if(!className){
+        alert("请输入班次名称！");
+
+    }else {
+        var urlStr = 'http://39.108.89.212:8080/security/schedule/add?name=' + className;
+      //  alert(urlStr)
+        $.ajax({
+            url: urlStr,
+            dataType: 'json',
+            success: function (obj) {
+                if (obj.code == 8) {
+                    alert('新增失败, 部门简称只能是唯一的2位字母！')
+                } else {
+                    alert(obj.message);
+                    getAllScheduleInformation()
+                }
+            },
+            error: function (error) {
+                console.log(error)
+            }
+        })
+    }
+}
+
+function addScheduleType() {
+    var classTypeName = $('#modal-classTypeName').val();
+    var classId = $('#modal-classId').val();
+    var  punchTime1= $('#modal-startPunchTime').val();;
+    var punchTime2 = $('#modal-goOffPunchTime').val();
+
+    $('#modal-startWorkTime').change(function(){
+
+        $('#modal-startWorkTime').attr("value",$(this).val()); //赋值
+    });
+
+    $('#modal-goOffWorkTime').change(function(){
+
+        $('#modal-goOffWorkTime').attr("value",$(this).val()); //赋值
+    });
+
+    $('#modal-startBreakTime').change(function(){
+
+        $('#modal-startBreakTime').attr("value",$(this).val()); //赋值
+    });
+
+    $('#modal-goOffBreakTime').change(function(){
+
+        $('#modal-goOffBreakTime').attr("value",$(this).val()); //赋值
+    });
+
+    var workTime1= $('#modal-startWorkTime').val();
+    var workTime2= $('#modal-goOffWorkTime').val();
+    var  breakTime1= $('#modal-startBreakTime').val();
+    var breakTime2 = $('#modal-goOffBreakTime').val();
+    if(!classTypeName){
+        alert("请输入班次类型名称！");
+
+    }else {
+        var urlStr = 'http://39.108.89.212:8080/security/scheduleType/add?name=' + classTypeName + '&startTime=' + workTime1 + '&endTime=' + workTime2 + '&beforeMinute=' + punchTime1 + '&afterMinute=' + punchTime2
+            + '&startBreakTime=' + breakTime1 + '&endBreakTime=' + breakTime2 + '&schedule=' + classId;
+        // http://39.108.89.212:8080/security/scheduleType/add?name=%E7%99%BD%E7%8F%AD&startTime=08:00&endTime=12:00&beforeMinute=5&afterMinute=5&startBreakTime=10:00&endBreakTime=10:30&schedule=1
+        alert(urlStr);
+        $.ajax({
+            url: urlStr,
+            dataType: 'json',
+            success: function (obj) {
+                if (obj.code == 8) {
+                    alert('新增失败, 部门简称只能是唯一的2位字母！')
+                } else {
+                    alert(obj.message);
+                    getAllScheduleInformation()
+                }
+            },
+            error: function (error) {
+                console.log(error)
+            }
+        })
+    }
+}
+
+function searchScheduleByName() {
+    if($('#page_num').val()==''){
+        var page=0
+    }else page =$('#page_num').val();
+    var size = 5;
+    var sortFieldName = 'id';
+    var asc = 1;
+    var name =$('#schedule_name').val();
+    var urlStr = 'http://39.108.89.212:8080/security/schedule/getByNameLikeByPage?page='+ page +'&name='+ name + '&size=' + size + '&sortFieldName=' + sortFieldName + '&asc=' + asc;
+    alert(urlStr);
+    $.ajax({
+        url:urlStr,
+        dataType:'json',
+        cache:false,
+        success:function (obj) {
+            setScheduleTableInformation(obj)
+        },
+        error:function (error) {
+            console.log(error)
+        }
+    })
+
+}
---
+++ src/main/resources/static/js/attendanceClassType.js
@@ -0,0 +1,119 @@
+$(document).ready(function () {
+    var activeNumbersOfWork;
+    $('.numbersOfWork').on('click', function () {
+        if(!activeNumbersOfWork){
+            activeNumbersOfWork = $(this);
+            activeNumbersOfWork.addClass('active')
+        }else{
+            var previousNumbersOfWork = activeNumbersOfWork;
+            previousNumbersOfWork.removeClass('active');
+            activeNumbersOfWork = $(this);
+            activeNumbersOfWork.addClass('active')
+        }
+
+    });
+
+    $('#select-all').on('click', function () {
+        if(this.checked == true){
+            $('.select-sub-box').prop('checked',true)
+        }
+        else{
+            $('.select-sub-box').prop('checked',false)
+        }
+    });
+    getAllScheduleInformation()
+});
+
+
+function getClassType(){
+    if($('#page_num').val()==''){
+      var  page=0;
+    }else page = $('#page_num').val()
+    var size = 5
+    var sortFieldName = 'id'
+    var asc = 1
+    var urlStr =  'http://39.108.89.212:8080/security/scheduleType/getAllByPage?page='+ page + '&size=' + size + '&sortFieldName=' + sortFieldName + '&asc=' + asc
+    alert(urlStr)
+    $.ajax({
+        url:urlStr,
+        dataType:'json',
+        success:function (obj) {
+            setScheduleType(obj)
+        },
+        error:function (error) {
+            console.log(error)
+        }
+    })
+}
+function setScheduleType(obj) {
+    var table_tr = $('.table-tr');
+    var st_id = $('.id');
+    var st_name = $('.name');
+    var worktime1 = $('.startWorkTime');
+    var worktime2 = $('.endWorkTime');
+    var btime = $('.beforeTime');
+    var atime = $('.afterTime');
+    var breaktime1 = $('.startBreakTime');
+    var breaktime2 = $('.endBreakTime');
+    var s_id= $('.schedule');
+    for(var i = 0; i < obj.data.numberOfElements; i++){
+        table_tr.eq(i).removeClass('hidden');
+        st_id.eq(i).html("<input class=\"select-box select-sub-box\" type=\"checkbox\"" +  "value='" + obj.data.content[i].id + "'" + ">"+obj.data.content[i].id);
+        st_name.eq(i).text(obj.data.content[i].name);
+        worktime1.eq(i).text(obj.data.content[i].startTime);
+        worktime2.eq(i).text(obj.data.content[i].endTime);
+        breaktime1.eq(i).text(obj.data.content[i].startBreakTime);
+        breaktime2.eq(i).text(obj.data.content[i].endBreakTime);
+        btime.eq(i).text(obj.data.content[i].beforeMinute);
+        atime.eq(i).text(obj.data.content[i].afterMinute);
+        s_id.eq(i).text(obj.data.content[i].schedule.id);
+
+    }
+    for (var i = obj.data.numberOfElements; i < 5; i++){
+        table_tr.eq(i).addClass('hidden')
+    }
+}
+
+function deleteClass(thisObj) {
+    var td = $(thisObj).parent().parent().find('td')
+    var classID = td.eq(0).text()
+    var urlStr =  'http://39.108.89.212:8080/security/scheduleType/deleteById?id='+ classID
+    $.ajax({
+        url:urlStr,
+        dataType:'json',
+        success:function (obj) {
+            if(obj.code == 0){
+                alert("删除成功！")
+                getClassType()
+            }else {
+                alert(obj.message)
+            }
+        },
+        error:function (error) {
+            console.log(error)
+        }
+    })
+}
+
+function searchCByName() {
+    if($('#page_num').val()==''){
+        var page=0
+    }else page =$('#page_num').val();
+    var size = 5;
+    var sortFieldName = 'id';
+    var asc = 1;
+    var name =$('#Cname').val();
+    var urlStr = 'http://39.108.89.212:8080/security/scheduleType/getByNameLikeByPage?page='+ page +'&name='+ name + '&size=' + size + '&sortFieldName=' + sortFieldName + '&asc=' + asc;
+    alert(urlStr);
+    $.ajax({
+        url:urlStr,
+        dataType:'json',
+        cache:false,
+        success:function (obj) {
+            setScheduleType(obj)
+        },
+        error:function (error) {
+            console.log(error)
+        }
+    })
+}
