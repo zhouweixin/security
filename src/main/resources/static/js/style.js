edcdfccbb414652fedcdc7427f9535a50a744d10
chanshibing <chanshibing@nuaa.edu.cn>
2018-07-18 18:32:59 +0800

    chanshibing

--- src/main/resources/static/js/style.js
+++ src/main/resources/static/js/style.js
@@ -1,31 +1,5 @@
 $(document).ready(function () {
-    /*
-       时间控件/
-     */
-    $('.form_date').datetimepicker({
-        format: "yyyy/mm/dd",
-        language: 'zh-CN',
-        pickerPosition: 'top-left',
-        weekStart: 1,
-        todayBtn: 0,
-        todayHighlight: 1,
-        startView: 2,
-        minView: 2,
-        forceParse: 0,
-        autoclose:1,
-    });
-    $('.to_date').datetimepicker({
-        format: "yyyy/mm/dd",
-        language: 'zh-CN',
-        pickerPosition: 'bottom-left',
-        weekStart: 1,
-        todayBtn: 1,
-        autoclose: 1,
-        todayHighlight: 1,
-        startView: 2,
-        minView: 2,
-        forceParse: 0
-    });
+
     $('#select-all').on('click', function () {
         if(this.checked == true){
             $('.select-sub-box').prop('checked',true)
