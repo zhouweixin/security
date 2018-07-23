bb4d3756649e7c053c52f9d503f438afa079039f
chanshibing <chanshibing@nuaa.edu.cn>
2018-07-23 20:12:39 +0800

    chanshibing

--- src/main/resources/static/js/warehouseCheck.js
+++ src/main/resources/static/js/warehouseCheck.js
@@ -1,3 +1,4 @@
+var currentPage = 0
 $(document).ready(function () {
     /*
     申请报损按钮/
@@ -28,8 +29,10 @@ $(document).ready(function () {
 获取所有库存/
  */
 function getAllStock() {
+    currentPage = 0
+    var page = currentPage
     $.ajax({
-        url: ipPort + '/stock/getAllByPage',
+        url: ipPort + '/stock/getAllByPage?page=' + page,
         success: function (obj) {
             if(obj.code == 0){
                 setAllStockTable(obj)
@@ -46,7 +49,8 @@ function getAllStock() {
 设置所有库存表/
  */
 function setAllStockTable(obj) {
-    console.log(obj)
+    $('#allWarehouseGoodsInformationPanel .currentPage').text(currentPage + 1)
+    $('#allWarehouseGoodsInformationPanel .totalPage').text(obj.data.totalPages)
     var id = $('.allWarehouseGoodsInformation-goodsId')
     var name = $('.allWarehouseGoodsInformation-goodsName')
     var unit = $('.allWarehouseGoodsInformation-unit')
@@ -71,9 +75,11 @@ function setAllStockTable(obj) {
 通过物品名称搜索/
  */
 function searchByName() {
+    currentPage = 0
+    var page = currentPage
     var name = $('#allWarehouseGoodsInformation-goodsName-input').val()
     $.ajax({
-        url:ipPort + '/stock/getByMaterialNameLikeByPage?name=' + name,
+        url:ipPort + '/stock/getByMaterialNameLikeByPage?name=' + name + '&page=' + page,
         success:function (obj) {
             if(obj.code == 0){
                 if(obj.data.numberOfElements == 0){
@@ -96,4 +102,103 @@ function searchByName() {
  */
 function setReportSpoiledPanel(thisObj) {
 
+}
+/*
+上一页/
+ */
+function previousPage(str) {
+    var currentPage_ = $(str).find('.currentPage').text()
+    if(currentPage_ == 1){
+        alert("已经是第一页！")
+        return
+    }
+    currentPage--
+    if(currentPage < 0){
+        currentPage = 0
+    }
+    var page = currentPage
+    var size = 10
+    var sortFieldName = 'id'
+    var asc = 1
+    var urlStr = ipPort + '/contract/getAllByPage?page='+ page + '&size=' + size + '&sortFieldName=' + sortFieldName + '&asc=' + asc
+    $.ajax({
+        url:urlStr,
+        dataType:'json',
+        success:function (obj) {
+            if(obj.code == 0){
+                setContractTableInformation(obj)
+            }else{
+                console.log(obj)
+            }
+        },
+        error:function (error) {
+            console.log(error)
+        }
+    })
+}
+/*
+下一页/
+ */
+function nextPage(str) {
+    var currentPage_ = $(str).find('.currentPage').text()
+    var totalPage_ = $(str).find('.totalPage').text()
+    if(currentPage_ == totalPage_){
+        alert("已经是最后一页！")
+        return
+    }
+    currentPage++
+    var page = currentPage
+    var size = 10
+    var sortFieldName = 'id'
+    var asc = 1
+    var urlStr = ipPort + '/contract/getAllByPage?page='+ page + '&size=' + size + '&sortFieldName=' + sortFieldName + '&asc=' + asc
+    $.ajax({
+        url:urlStr,
+        dataType:'json',
+        success:function (obj) {
+            if(obj.code == 0){
+                setContractTableInformation(obj)
+            }else{
+                console.log(obj)
+            }
+        },
+        error:function (error) {
+            console.log(error)
+        }
+    })
+}
+/*
+跳转页/
+ */
+function skipPage(str) {
+    var skipPage_ = parseInt( $(str).find('.skipPage').val())
+    var totalPage_ = parseInt( $(str).find('.totalPage').text())
+    if(skipPage_ - totalPage_ > 0){
+        alert("没有此页！")
+        return
+    }
+    if(skipPage_ < 1){
+        alert("没有此页！")
+        return
+    }
+    currentPage = skipPage_ - 1
+    var page = currentPage
+    var size = 10
+    var sortFieldName = 'id'
+    var asc = 1
+    var urlStr = ipPort + '/contract/getAllByPage?page='+ page + '&size=' + size + '&sortFieldName=' + sortFieldName + '&asc=' + asc
+    $.ajax({
+        url:urlStr,
+        dataType:'json',
+        success:function (obj) {
+            if(obj.code == 0){
+                setContractTableInformation(obj)
+            }else{
+                console.log(obj)
+            }
+        },
+        error:function (error) {
+            console.log(error)
+        }
+    })
 }
