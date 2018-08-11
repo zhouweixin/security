7f0bbb4a24990c1e035eb93aac984441d1fcaca9
chanshibing <chanshibing@nuaa.edu.cn>
2018-08-11 16:24:10 +0800

    chanshibing

---
+++ src/main/resources/static/js/salaryQuery.js
@@ -0,0 +1,12 @@
+/*
+打印薪资表/
+ */
+function printSalaryTable() {
+    $('#myModal-printInit').modal('toggle')
+    if($('.printAll-box').checked == true){//如果全部打印为真
+
+    }
+    else{//为否
+
+    }
+}
